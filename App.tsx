b5ceb62d07f7ef181b8f00a30684122e01842237
Antonio Cosentino <hello@antoniocosentino.it>
2020-06-10 17:59:56 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the UI Kitten TypeScript template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  ImageProps,
+  StyleSheet,
+} from 'react-native';
+import {
+  ApplicationProvider,
+  Button,
+  Icon,
+  IconRegistry,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+
+/**
+ * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
+ * https://akveo.github.io/eva-icons
+ */
+const StartIcon = (props?: Partial<ImageProps>): React.ReactElement<ImageProps> => (
+  <Icon {...props} name='arrow-ios-forward-outline'/>
+);
+
+export default (): React.ReactFragment => (
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <Text style={styles.text} category='h1'>
+          FlipCards
+        </Text>
+        <Text style={styles.text} appearance='hint'>
+          Hello World
+        </Text>
+        <Button style={styles.likeButton} accessoryLeft={StartIcon}>
+          START
+        </Button>
+      </Layout>
+    </ApplicationProvider>
+  </>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  likeButton: {
+    marginVertical: 16,
+  },
+});
