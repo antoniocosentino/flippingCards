063a8ed8bd04f5d17b6bdbc08c26f4a81baea885
Antonio Cosentino <hello@antoniocosentino.it>
2020-06-16 00:22:40 +0200

    kickstarting the project

--- App.tsx
+++ App.tsx
@@ -1,68 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten TypeScript template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
- * @format
- */
-
 import React from 'react';
 import {
-  ImageProps,
-  StyleSheet,
-} from 'react-native';
-import {
-  ApplicationProvider,
-  Button,
+  BottomNavigation,
+  BottomNavigationTab,
   Icon,
+  ApplicationProvider,
   IconRegistry,
   Layout,
   Text,
 } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+import { StyleSheet } from 'react-native';
+
 import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
-const StartIcon = (props?: Partial<ImageProps>): React.ReactElement<ImageProps> => (
-  <Icon {...props} name='arrow-ios-forward-outline'/>
-);
 
-export default (): React.ReactFragment => (
-  <>
-    <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
-          FlipCards
-        </Text>
-        <Text style={styles.text} appearance='hint'>
-          Hello World
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={StartIcon}>
-          START
-        </Button>
-      </Layout>
-    </ApplicationProvider>
-  </>
-);
+const ListIcon = (props) => <Icon {...props} name="list-outline" />;
+const CardsIcon = (props) => <Icon {...props} name="grid-outline" />;
+const PlusIcon = (props) => <Icon {...props} name="plus-circle-outline" />;
+const PlayIcon = (props) => <Icon {...props} name="award-outline" />;
+const SettingsIcon = (props) => <Icon {...props} name="settings-2-outline" />;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-  },
-  likeButton: {
-    marginVertical: 16,
-  },
-});
+    text: {
+      textAlign: 'center',
+      color: '#000000'
+    },
+  });
+
+export default () => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Layout
+          style={{ flex: 11, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={styles.text} category='h1'>FlipCards</Text>
+        </Layout>
+        <Layout
+          style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', paddingBottom: 20 }}>
+          <BottomNavigation
+            selectedIndex={selectedIndex}
+            onSelect={(index) => setSelectedIndex(index)}>
+            <BottomNavigationTab icon={ListIcon} />
+            <BottomNavigationTab icon={CardsIcon} />
+            <BottomNavigationTab icon={PlusIcon} />
+            <BottomNavigationTab icon={PlayIcon} />
+            <BottomNavigationTab icon={SettingsIcon} />
+          </BottomNavigation>
+        </Layout>
+      </ApplicationProvider>
+    </>
+  );
+};
