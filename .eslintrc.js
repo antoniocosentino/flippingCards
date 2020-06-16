c80594cfba1befeee04fd9b255b7d15acef6ddcc
Antonio Cosentino <hello@antoniocosentino.it>
2020-06-16 23:34:59 +0200

    lots of cleanups + removed prettier
    because prettier is shittier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,11 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'jsx-quotes': [ 1, 'prefer-single' ],
+    'prettier/prettier': 0,
+    'space-in-parens': [ 1, 'always' ],
+    'indent': [ 1, 4 ],
+    'comma-dangle': [ 1, 'never' ]
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {
-  BottomNavigation,
-  BottomNavigationTab,
-  Icon,
-  ApplicationProvider,
-  IconRegistry,
-  Layout,
-  Text,
+    BottomNavigation,
+    BottomNavigationTab,
+    Icon,
+    ApplicationProvider,
+    IconRegistry,
+    Layout,
+    Text,
+    IconProps
 } from '@ui-kitten/components';
 
 import { StyleSheet } from 'react-native';
@@ -14,44 +15,56 @@ import { StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
+type TIconProps = any;
 
-const ListIcon = (props) => <Icon {...props} name="list-outline" />;
-const CardsIcon = (props) => <Icon {...props} name="grid-outline" />;
-const PlusIcon = (props) => <Icon {...props} name="plus-circle-outline" />;
-const PlayIcon = (props) => <Icon {...props} name="award-outline" />;
-const SettingsIcon = (props) => <Icon {...props} name="settings-2-outline" />;
 
-const styles = StyleSheet.create({
-    text: {
-      textAlign: 'center',
-      color: '#000000'
+const ListIcon = ( props: IconProps ) => <Icon {...props} name='list-outline' />;
+const CardsIcon = ( props: IconProps ) => <Icon {...props} name='grid-outline' />;
+const PlusIcon = ( props: IconProps ) => <Icon {...props} name='plus-circle-outline' />;
+const PlayIcon = ( props: IconProps ) => <Icon {...props} name='award-outline' />;
+const SettingsIcon = ( props: IconProps ) => <Icon {...props} name='settings-2-outline' />;
+
+const styles = StyleSheet.create( {
+    mainBlock: {
+        flex: 11,
+        justifyContent:
+        'center',
+        alignItems: 'center'
     },
-  });
+    bottomZone: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        paddingBottom: 20
+    },
+    text: {
+        textAlign: 'center',
+        color: '#000000'
+    }
+} );
 
 export default () => {
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [selectedIndex, setSelectedIndex] = React.useState( 0 );
 
-  return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Layout
-          style={{ flex: 11, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={styles.text} category='h1'>FlipCards</Text>
-        </Layout>
-        <Layout
-          style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', paddingBottom: 20 }}>
-          <BottomNavigation
-            selectedIndex={selectedIndex}
-            onSelect={(index) => setSelectedIndex(index)}>
-            <BottomNavigationTab icon={ListIcon} />
-            <BottomNavigationTab icon={CardsIcon} />
-            <BottomNavigationTab icon={PlusIcon} />
-            <BottomNavigationTab icon={PlayIcon} />
-            <BottomNavigationTab icon={SettingsIcon} />
-          </BottomNavigation>
-        </Layout>
-      </ApplicationProvider>
-    </>
-  );
+    return (
+        <>
+            <IconRegistry icons={EvaIconsPack} />
+            <ApplicationProvider {...eva} theme={eva.light}>
+                <Layout style={ styles.mainBlock }>
+                    <Text style={styles.text} category='h1'>FlipCards</Text>
+                </Layout>
+                <Layout style={ styles.bottomZone }>
+                    <BottomNavigation
+                        selectedIndex={selectedIndex}
+                        onSelect={ ( index ) => setSelectedIndex( index ) }>
+                        <BottomNavigationTab icon={ListIcon} />
+                        <BottomNavigationTab icon={CardsIcon} />
+                        <BottomNavigationTab icon={PlusIcon} />
+                        <BottomNavigationTab icon={PlayIcon} />
+                        <BottomNavigationTab icon={SettingsIcon} />
+                    </BottomNavigation>
+                </Layout>
+            </ApplicationProvider>
+        </>
+    );
 };
