68f84b98325ef66d8c5e3355862114c3f0b38880
Antonio Cosentino <hello@antoniocosentino.it>
2020-09-01 23:53:10 +0200

    cleanup

--- App.tsx
+++ App.tsx
@@ -13,12 +13,13 @@ import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import AsyncStorage from '@react-native-community/async-storage';
 import { DEMO_WORDS } from './utils/demoData';
-import { styles, mainColor } from './styles/styles';
+import { styles } from './styles/styles';
 import { List } from './views/List';
 import { BottomMenu } from './views/BottomMenu';
 import { SearchResults } from './components/SearchResults';
 import { removeArticle, getShuffledCards } from './utils/utils';
 import { Cards } from './views/Cards';
+import { customTheme } from './utils/customTheme';
 
 const SQLite = require( 'react-native-sqlite-storage' );
 
@@ -30,15 +31,7 @@ const errorCallback = ( error: any ) => {
     console.log( 'DB connection error', error );
 };
 
-// TODO: if it's Android I need to use 2 as parameter and re-create the DB manually
 const db = SQLite.openDatabase( { name: 'dictionary.db', createFromLocation: 1 }, okCallback, errorCallback );
-
-
-// use this snippet to delete the database on Android
-// SQLite.deleteDatabase( {
-//     name: 'dictionary.db',
-//     location: 'default'
-// } );
 
 export type TSingleWord = {
     de: string,
@@ -221,70 +214,6 @@ export default () => {
                 <Icon { ...props } width={ 22 } height={ 22 } fill='#ccc' name={ 'close-circle' } />
             </TouchableWithoutFeedback>
         );
-    };
-
-    const colorOverrides = {
-        'color-basic-100': '#FFFFFF',
-        'color-basic-1000': '#151A30',
-        'color-basic-1100': '#101426',
-        'color-basic-200': '#F7F9FC',
-        'color-basic-300': '#EDF1F7',
-        'color-basic-400': '#E4E9F2',
-        'color-basic-500': '#C5CEE0',
-        'color-basic-600': '#8F9BB3',
-        'color-basic-700': '#2E3A59',
-        'color-basic-800': '#222B45',
-        'color-basic-900': '#1A2138',
-        'color-danger-100': '#FFF2F2',
-        'color-danger-200': '#FFD6D9',
-        'color-danger-300': '#FFA8B4',
-        'color-danger-400': '#FF708D',
-        'color-danger-500': '#FF3D71',
-        'color-danger-600': '#DB2C66',
-        'color-danger-700': '#B81D5B',
-        'color-danger-800': '#94124E',
-        'color-danger-900': '#700940',
-        'color-info-100': '#F2F8FF',
-        'color-info-200': '#C7E2FF',
-        'color-info-300': '#94CBFF',
-        'color-info-400': '#42AAFF',
-        'color-info-500': '#0095FF',
-        'color-info-600': '#006FD6',
-        'color-info-700': '#0057C2',
-        'color-info-800': '#0041A8',
-        'color-info-900': '#002885',
-        'color-primary-100': '#F2F6FF',
-        'color-primary-200': '#D9E4FF',
-        'color-primary-300': '#A6C1FF',
-        'color-primary-400': '#598BFF',
-        'color-primary-500': mainColor,
-        'color-primary-600': mainColor, // this is the onPress color. Can be customized
-        'color-primary-700': '#1A34B8',
-        'color-primary-800': '#102694',
-        'color-primary-900': '#091C7A',
-        'color-success-100': '#EDFFF3',
-        'color-success-200': '#B3FFD6',
-        'color-success-300': '#8CFAC7',
-        'color-success-400': '#51F0B0',
-        'color-success-500': '#00E096',
-        'color-success-600': '#00B383',
-        'color-success-700': '#008F72',
-        'color-success-800': '#007566',
-        'color-success-900': '#00524C',
-        'color-warning-100': '#FFFDF2',
-        'color-warning-200': '#FFF1C2',
-        'color-warning-300': '#FFE59E',
-        'color-warning-400': '#FFC94D',
-        'color-warning-500': '#FFAA00',
-        'color-warning-600': '#DB8B00',
-        'color-warning-700': '#B86E00',
-        'color-warning-800': '#945400',
-        'color-warning-900': '#703C00'
-    };
-
-    const customTheme = {
-        ...eva.light,
-        ...colorOverrides
     };
 
     const [ stateCards, setStateCards ] = useState( [] as TSearchWords );
