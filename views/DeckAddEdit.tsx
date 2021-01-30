ba1add9cd223854ccd2d391ffb25a0363ad944eb
Antonio Cosentino <hello@antoniocosentino.it>
2021-01-30 19:16:44 +0100

    new color scheme + general styling improvements

--- views/DeckAddEdit.tsx
+++ views/DeckAddEdit.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 
 import { Layout, Text } from '@ui-kitten/components';
 import { styles } from '../styles/styles';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const DeckAddEdit = () => {
 
     return (
-        <SafeAreaView style={ styles.mainViewWrapper }>
-            <Layout style={ [ styles.centeredElement, styles['centeredElement--noTopSpace' ] ] }>
-                <Text style={ [ styles.text, styles.titleText ] } category='h4'>Create New Deck</Text>
+        <Layout style={ [ styles.centeredElement, styles['centeredElement--noTopSpace' ], styles['centeredElement--lessHorizontalPadding' ] ] }>
+            <Text style={ [ styles.text, styles.titleText ] } category='h4'>Create New Deck</Text>
 
-                <Text style={ [ styles.text, styles.instructionsText ] }>
-                    here there will be things
-                </Text>
-            </Layout>
-        </SafeAreaView>
+            <Text style={ styles.text }>
+                Select words that will be added to this deck.
+            </Text>
+        </Layout>
     );
 };
