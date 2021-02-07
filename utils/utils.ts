1d1cf6c551a7f9854f5f5be81b9145adcfeaba1a
Antonio Cosentino <hello@antoniocosentino.it>
2021-02-07 21:59:44 +0100

    checkbox logic (working)

--- utils/utils.ts
+++ utils/utils.ts
@@ -167,3 +167,38 @@ const getWalletWordsInGivenPeriod = ( words: TWordsWallet, wordsFreshness: TWord
     return wordsResult;
 
 };
+
+export const timeAgo = ( date: any ) =>  {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = Date.now() - date;
+
+    if ( elapsed < msPerMinute ) {
+        return Math.round( elapsed / 1000 ) + ' seconds ago';
+    }
+
+    else if ( elapsed < msPerHour ) {
+        return Math.round( elapsed / msPerMinute ) + ' minutes ago';
+    }
+
+    else if ( elapsed < msPerDay ) {
+        return Math.round( elapsed / msPerHour ) + ' hours ago';
+    }
+
+    else if ( elapsed < msPerMonth ) {
+        return Math.round( elapsed / msPerDay ) + ' days ago';
+    }
+
+    else if ( elapsed < msPerYear ) {
+        return Math.round( elapsed / msPerMonth ) + ' months ago';
+    }
+
+    else {
+        return Math.round( elapsed / msPerYear ) + ' years ago';
+    }
+};
