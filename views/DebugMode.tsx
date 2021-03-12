4633d77747c8d52381303505a6b1bed7ff3f2c2d
Antonio Cosentino <hello@antoniocosentino.it>
2021-03-12 22:40:13 +0100

    fixed loading behaviour

--- views/DebugMode.tsx
+++ views/DebugMode.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, Button, Layout } from '@ui-kitten/components';
-import { AppContext, dbRefresh } from '../App';
+import { AppContext, dbRefresh, enrichDecksWithPlaceholder } from '../App';
 import { DEMO_WORDS_DEBUG } from '../utils/demoData.debug';
 import { storeDBversion } from '../App';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -11,42 +11,60 @@ import { BottomMenu } from './BottomMenu';
 export const DebugMode = () => {
 
     const appData = useContext( AppContext );
-    const { storeData, storeDecksData } = appData;
+    const { storeData, storeDecksData, setWordsWallet, setDecksData } = appData;
 
     return (
         <Layout style={ styles.megaWrap } >
             <SafeAreaView style={ styles.mainViewWrapper }>
                 <Layout style={ styles.centeredElement }>
                     <Button
-                        onPress={ () => storeData( DEMO_WORDS_DEBUG ) }
+                        onPress={ () => {
+                            storeData( DEMO_WORDS_DEBUG ).then( () => {
+                                setWordsWallet( DEMO_WORDS_DEBUG );
+                            } );
+                        } }
                     >
-                        Fill Wallet with Debug Demo Words
+                        Fill Wallet with Demo Words
                     </Button>
 
                     <Text />
 
                     <Button
-                        onPress={ () => storeDecksData( DEMO_DECKS ) }
-                    >
-                        Fill with demo decks
-                    </Button>
-
-                    <Text />
-
-                    <Button
-                        onPress={ () => storeData( [] ) }
+                        onPress={ () => {
+                            storeData( [] ).then( () => {
+                                setWordsWallet( [] );
+                            } );
+                        } }
                     >
                         Wipe Wallet
                     </Button>
 
                     <Text />
+                    <Text />
 
                     <Button
-                        onPress={ () => storeDecksData( [] ) }
+                        onPress={ () => {
+                            storeDecksData( DEMO_DECKS ).then( () => {
+                                setDecksData( enrichDecksWithPlaceholder( DEMO_DECKS ) );
+                            } );
+                        } }
+                    >
+                        Fill Decks with Demo Decks
+                    </Button>
+
+                    <Text />
+
+                    <Button
+                        onPress={ () => {
+                            storeDecksData( [] ).then( () => {
+                                setDecksData( [] );
+                            } );
+                        } }
                     >
                         Wipe Decks
                     </Button>
 
+                    <Text />
                     <Text />
 
                     <Button
