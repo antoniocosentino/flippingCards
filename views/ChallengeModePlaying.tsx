7f6e1511dee185e22dcc195f92defb9153615f7b
Antonio Cosentino <hello@antoniocosentino.it>
2021-03-24 23:11:23 +0100

    setting up challenge mode - playing mode

---
+++ views/ChallengeModePlaying.tsx
@@ -0,0 +1,13 @@
+import { Layout, Text } from '@ui-kitten/components';
+import React from 'react';
+
+type TChallengeModePlaying = any;
+
+export const ChallengeModePlaying = ( props: TChallengeModePlaying ) => {
+
+    return (
+        <Layout>
+            <Text>hello there</Text>
+        </Layout>
+    );
+};
