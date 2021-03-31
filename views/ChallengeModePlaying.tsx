3518a81e26b93f0a882906861626f8ed5c0717ca
Antonio Cosentino <hello@antoniocosentino.it>
2021-03-31 22:18:07 +0200

    initial implementation of words input

--- views/ChallengeModePlaying.tsx
+++ views/ChallengeModePlaying.tsx
@@ -1,14 +1,44 @@
 import { Layout, Text, Button } from '@ui-kitten/components';
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../App';
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext, TCards, TSingleWord } from '../App';
 import { styles, mainColor } from '../styles/styles';
 import { Dimensions } from 'react-native';
+import CharacterInput from 'react-native-character-input';
+import { shuffle } from 'lodash';
+import { getArticle } from '../utils/utils';
 
 type TChallengeModePlaying = any;
 
 type TProgressBar = {
     totalNumber: number;
     currentNumber: number;
+};
+
+const getWordToGuessAsArray = ( word: TSingleWord ): string[] => {
+
+    const fullWord = `${ getArticle( word ) }${word.de}`;
+
+    return fullWord.split( '' );
+};
+
+const getWordBinary = ( letterArray: ReadonlyArray<string> ): string => {
+
+    const binaryArr = letterArray.map( ( thisLetter ) => {
+        if ( thisLetter !== ' ' ) {
+            return '1';
+        } else {
+            return '0';
+        }
+    } );
+
+    return binaryArr.join( '' );
+};
+
+const getWordPlaceholder = ( letterArray: ReadonlyArray<string> ): string => {
+
+    const placeholderArr = letterArray.map( () => ' ' );
+
+    return placeholderArr.join( '' );
 };
 
 const ProgressBar = ( props: TProgressBar ) => {
@@ -51,6 +81,13 @@ export const ChallengeModePlaying = ( props: TChallengeModePlaying ) => {
     const { decksData } = appData;
 
     const currentDeck = decksData.find( ( deck ) => deck.id === deckKey );
+    const currentDeckCards = ( currentDeck?.cards || [] );
+
+    const [ currentDeckCardsShuffled, setShuffledCards ] = useState( [] as TCards );
+
+    useEffect( () => {
+        setShuffledCards( shuffle( currentDeckCards ) );
+    }, [ currentDeckCards ] );
 
     const [ currentCard, setCurrentCard ] = useState( 0 );
 
@@ -58,22 +95,39 @@ export const ChallengeModePlaying = ( props: TChallengeModePlaying ) => {
         setCurrentCard( currentCard + 1 );
     };
 
+    const [ typedWord, setTypedWord ] = useState( [] as ReadonlyArray<string> );
+    const currentWord = currentDeckCardsShuffled[ currentCard ];
+
+    if ( !currentWord ) {
+        return null;
+    }
+
+    const wordToGuessAsArray = getWordToGuessAsArray( currentWord );
+    console.log("🍌: ChallengeModePlaying -> wordToGuessAsArray", wordToGuessAsArray)
+    const wordBinary = getWordBinary( wordToGuessAsArray );
+    const placeHolder = getWordPlaceholder( wordToGuessAsArray );
+
     return (
         <Layout style={ styles['centeredElement--mediumHorizontalPadding'] }>
 
             <ProgressBar
-                totalNumber={ currentDeck?.cards.length || 0 }
+                totalNumber={ currentDeckCards.length || 0 }
                 currentNumber={ currentCard }
             />
 
             <Layout style={ styles.verticalSpacer } />
 
             <Layout>
-                <Text style={ [ styles.text, styles.verySmallText] } >{ currentCard + 1 }/{ currentDeck?.cards.length || 0 }</Text>
-                <Text style={ [ styles.text, styles.veryBigText, styles.boldText ] } >{ currentDeck?.cards?.[ currentCard ].en  }</Text>
+                <Text style={ [ styles.text, styles.verySmallText] } >{ currentCard + 1 }/{ currentDeckCardsShuffled.length || 0 }</Text>
+                <Text style={ [ styles.text, styles.veryBigText, styles.boldText ] } >{ currentDeckCardsShuffled[ currentCard ].en  }</Text>
                 <Text>{ '\n' }</Text>
-                <Text style={ [ styles.text, styles.verySmallText, styles.lightText ] } >Type it in German:</Text>
-                <Text style={ [ styles.text, styles.centeredText, styles.veryBigText ] }>_ _ _ _ _ _</Text>
+                <Text style={ [ styles.text, styles.verySmallText, styles.lightText ] } >Type it in German (article included):</Text>
+
+                <CharacterInput
+                    placeHolder={ placeHolder }
+                    showCharBinary={ wordBinary }
+                    handleChange={ ( value: ReadonlyArray<string> ) => setTypedWord( value ) }
+                />
 
                 <Layout style={ styles.verticalSpacer } />
 
