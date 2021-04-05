61f1e478ac4ce49c320f091f925a744cb624f306
Antonio Cosentino <hello@antoniocosentino.it>
2021-04-05 20:28:52 +0200

    improved message when there are no decks

--- views/ChallengeMode.tsx
+++ views/ChallengeMode.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { Card, Icon, IconProps, Layout, Text } from '@ui-kitten/components';
+import { Button, Card, Icon, IconProps, Layout, Text } from '@ui-kitten/components';
 import { mainColor, styles } from '../styles/styles';
 import { BottomMenu } from './BottomMenu';
 import { AppContext, TDeck } from '../App';
@@ -42,7 +42,7 @@ type TChallengeModeMainScreen = {
 export const ChallengeModeMainScreen = ( props: TChallengeModeMainScreen ) => {
 
     const appData = useContext( AppContext );
-    const { decksData } = appData;
+    const { decksData, onMenuClick } = appData;
     const { navigation } = props;
 
     const playClick = ( deckKey: number ) => {
@@ -53,79 +53,97 @@ export const ChallengeModeMainScreen = ( props: TChallengeModeMainScreen ) => {
         <Layout style={ styles.megaWrap } >
             <Layout style={ styles['centeredElement--lessHorizontalPadding'] }>
                 <Text style={ [ styles.text, styles.titleText ] } category='h4'>Challenge mode</Text>
-                <Text style={ [ styles.text, styles.smallerText ] }>Select a deck and start the challenge</Text>
-                <FlatList
-                    keyboardDismissMode={ 'on-drag' }
-                    showsVerticalScrollIndicator={ false }
-                    data={ decksData }
-                    style={ [ styles.cardsScrollView, styles.inputExtraTopSpacing ] }
-                    keyExtractor={ deck => deck.id.toString() }
-                    renderItem={ ( data: TWrappedDeck ) => {
+                { decksData.length <= 1 &&
+                    <Layout style={ styles.centeredElement }>
+                        <Text style={ [ styles.text, styles.smallerText, styles.lightText ] } >
+                            In this area you will be able to test your knowledge on words coming from your decks.
+                            Start by creating a deck in the Training Mode view and then come back 🙂
+                        </Text>
+                        <Layout style={ styles.walletInstructions }>
+                            <Button onPress={ () => { onMenuClick( 1 ); } } style={ styles.ctaButton }>
+                                GO TO TRANING MODE
+                            </Button>
+                        </Layout>
+                    </Layout>
+                }
 
-                        const { item: deck } = data;
+                { decksData.length > 1 &&
+                    <>
+                        <Text style={ [ styles.text, styles.smallerText ] }>Select a deck and start the challenge</Text>
+                        <FlatList
+                            keyboardDismissMode={ 'on-drag' }
+                            showsVerticalScrollIndicator={ false }
+                            data={ decksData }
+                            style={ [ styles.cardsScrollView, styles.inputExtraTopSpacing ] }
+                            keyExtractor={ deck => deck.id.toString() }
+                            renderItem={ ( data: TWrappedDeck ) => {
 
-                        if ( deck.name === '__ADD_PLACEHOLDER__' ) {
-                            return null;
-                        }
+                                const { item: deck } = data;
 
-                        return (
-                            <Card
-                                style={ styles.wordCard }
-                                onPress={ () => playClick( deck.id ) }
-                            >
-                                <Layout
-                                    style={ styles.challengeModeCardWrapper }
-                                >
-                                    <Layout
-                                        style={ styles.challengeModeCardLeftZone }
+                                if ( deck.name === '__ADD_PLACEHOLDER__' ) {
+                                    return null;
+                                }
+
+                                return (
+                                    <Card
+                                        style={ styles.wordCard }
+                                        onPress={ () => playClick( deck.id ) }
                                     >
                                         <Layout
-                                            style={ styles.challengeModeCardLeftZoneTop  }
+                                            style={ styles.challengeModeCardWrapper }
                                         >
-                                            <Text style={ [styles.text, styles.whiteText, styles.leftAlignedText ] }>
-                                                { deck.name  }
-                                            </Text>
+                                            <Layout
+                                                style={ styles.challengeModeCardLeftZone }
+                                            >
+                                                <Layout
+                                                    style={ styles.challengeModeCardLeftZoneTop  }
+                                                >
+                                                    <Text style={ [styles.text, styles.whiteText, styles.leftAlignedText ] }>
+                                                        { deck.name  }
+                                                    </Text>
+                                                </Layout>
+
+                                                <Layout
+                                                    style={ styles.challengeModeCardLeftZoneBottom }
+                                                >
+                                                    <Text style={ [styles.text, styles.whiteText, styles.leftAlignedText, styles.smallerText ] }>
+                                                        { `${ deck.cards.length } card${ deck.cards.length > 1 ? 's' : '' }` }
+                                                    </Text>
+                                                </Layout>
+                                            </Layout>
+
+                                            <Layout
+                                                style={ styles.challengeModeCardCenterZone }
+                                            >
+                                                <Pie
+                                                    radius={ 24 }
+                                                    innerRadius={ 18 }
+                                                    sections={ [
+                                                        {
+                                                            percentage: getDeckPercentage( deck ),
+                                                            color: '#FFF'
+                                                        }
+                                                    ] }
+                                                    backgroundColor='#DC9CAE'
+                                                />
+
+                                                <Text style={ [styles.text, styles.whiteText, styles.verySmallText, styles.challengeModeCardPercentageText ] }>
+                                                    { `${getDeckPercentage( deck )}%` }
+                                                </Text>
+
+                                            </Layout>
+
+                                            <Layout
+                                                style={ styles.challengeModeCardRightZone }
+                                            >
+                                                <PlayIcon />
+                                            </Layout>
                                         </Layout>
-
-                                        <Layout
-                                            style={ styles.challengeModeCardLeftZoneBottom }
-                                        >
-                                            <Text style={ [styles.text, styles.whiteText, styles.leftAlignedText, styles.smallerText ] }>
-                                                { `${ deck.cards.length } card${ deck.cards.length > 1 ? 's' : '' }` }
-                                            </Text>
-                                        </Layout>
-                                    </Layout>
-
-                                    <Layout
-                                        style={ styles.challengeModeCardCenterZone }
-                                    >
-                                        <Pie
-                                            radius={ 24 }
-                                            innerRadius={ 18 }
-                                            sections={ [
-                                                {
-                                                    percentage: getDeckPercentage( deck ),
-                                                    color: '#FFF'
-                                                }
-                                            ] }
-                                            backgroundColor='#DC9CAE'
-                                        />
-
-                                        <Text style={ [styles.text, styles.whiteText, styles.verySmallText, styles.challengeModeCardPercentageText ] }>
-                                            { `${getDeckPercentage( deck )}%` }
-                                        </Text>
-
-                                    </Layout>
-
-                                    <Layout
-                                        style={ styles.challengeModeCardRightZone }
-                                    >
-                                        <PlayIcon />
-                                    </Layout>
-                                </Layout>
-                            </Card>
-                        ); } }
-                />
+                                    </Card>
+                                ); } }
+                        />
+                    </>
+                }
             </Layout>
             <BottomMenu />
         </Layout>
